56e56090820b024bee88acbba98cfa5aeec37a5a
Shahnawaz Khan <shahnawazkhan96850@gmail.com>
2025-11-10 14:47:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/String Functions I - Python/string-functions-i-python.py
@@ -0,0 +1,17 @@
+def trim(str):
+    trim_str = str.strip()
+    return trim_str
+
+
+def exists(str, x):
+    Find = str.find(x)
+    return Find
+
+
+def titleIt(str):
+    Tilte = str.title()
+    return Tilte
+
+
+def casesSwap(str):
+    return str.swapcase()
