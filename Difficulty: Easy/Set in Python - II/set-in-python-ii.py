10d0ba496458482ec46fc99aed6c3193cdeff5f7
Shahnawaz Khan <shahnawazkhan96850@gmail.com>
2025-11-11 15:41:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Set in Python - II/set-in-python-ii.py
@@ -0,0 +1,25 @@
+# Function to find common elements in sets
+# should return the intersection of sets
+def common_in_set(a, b):
+    # Your code here
+    Inter = a.intersection(b)
+    return Inter
+
+# Function to find difference in sets
+# Should return the difference in sets
+def diff(a, b):
+    # Your code here
+    Diff = a.difference(b)
+    return Diff
+    
+
+
+
+# Function to find union of sets
+# Should return the union of sets
+def all_in_set(a, b):
+    # Your code here
+    All = a.union(b)
+    return All
+    
+    
